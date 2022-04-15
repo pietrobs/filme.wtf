f87ff482b9c1d2a5e7ac94b1b2d7748fbb85f9fb
Pietro Schiavinato <pietro.schiavinato@unesp.br>
2022-04-15 14:01:28 -0300

    (feat): Comment levenshteinDistance function

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,34 +1,34 @@
 // Thanks andrei-m https://gist.github.com/andrei-m/982927#file-levenshtein-js
 
-function levenshteinDistance(a, b) {
-    if (a.length == 0) return b.length;
-    if (b.length == 0) return a.length;
+// function levenshteinDistance(a, b) {
+//     if (a.length == 0) return b.length;
+//     if (b.length == 0) return a.length;
 
-    var matrix = [];
+//     var matrix = [];
 
-    var i;
-    for (i = 0; i <= b.length; i++) {
-        matrix[i] = [i];
-    }
-    var j;
-    for (j = 0; j <= a.length; j++) {
-        matrix[0][j] = j;
-    }
+//     var i;
+//     for (i = 0; i <= b.length; i++) {
+//         matrix[i] = [i];
+//     }
+//     var j;
+//     for (j = 0; j <= a.length; j++) {
+//         matrix[0][j] = j;
+//     }
 
-    for (i = 1; i <= b.length; i++) {
-        for (j = 1; j <= a.length; j++) {
-            if (b.charAt(i - 1) == a.charAt(j - 1)) {
-                matrix[i][j] = matrix[i - 1][j - 1];
-            } else {
-                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1,
-                    Math.min(matrix[i][j - 1] + 1,
-                        matrix[i - 1][j] + 1));
-            }
-        }
-    }
+//     for (i = 1; i <= b.length; i++) {
+//         for (j = 1; j <= a.length; j++) {
+//             if (b.charAt(i - 1) == a.charAt(j - 1)) {
+//                 matrix[i][j] = matrix[i - 1][j - 1];
+//             } else {
+//                 matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1,
+//                     Math.min(matrix[i][j - 1] + 1,
+//                         matrix[i - 1][j] + 1));
+//             }
+//         }
+//     }
 
-    return matrix[b.length][a.length];
-};
+//     return matrix[b.length][a.length];
+// };
 
 const gameObject = [
     {
